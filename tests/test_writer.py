8f974d3736bf7ce7cad5eb4d93fc212bc5e6e98a
Goran Stevanović <stevanovicgoran@proton.me>
2026-07-08 09:18:09 +0200

    fix: test generated tests contain selected questions

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -75,16 +75,16 @@ def sample_subject_3():
 @pytest.fixture
 def sample_questions():
     return {
-        "pitanje1": "1. Naziv prve oblasti: Prvo pitanje",
+        "pitanje1": "Naziv prve oblasti: Prvo pitanje",
         "pitanje2": "Naziv prve oblasti: Drugo pitanje",
         "pitanje3": "Naziv prve oblasti: Treće pitanje",
-        "pitanje4": "2. Naziv prve oblasti: Četvrto pitanje",
+        "pitanje4": "Naziv prve oblasti: Četvrto pitanje",
         "pitanje5": "Naziv prve oblasti: Peto pitanje",
         "pitanje6": "Naziv prve oblasti: Šesto pitanje",
-        "pitanje7": "3. Naziv prve oblasti: Sedmo pitanje",
+        "pitanje7": "Naziv prve oblasti: Sedmo pitanje",
         "pitanje8": "Naziv prve oblasti: Osmo pitanje",
         "pitanje9": "Naziv prve oblasti: Deveto pitanje",
-        "pitanje10": "4. Naziv prve oblasti: Deseto pitanje",
+        "pitanje10": "Naziv prve oblasti: Deseto pitanje",
     }
 
 
@@ -92,16 +92,16 @@ def sample_questions():
 @pytest.fixture
 def sample_answers():
     return {
-        "odgovor1": "1. Naziv prve oblasti: Prvo pitanje - odgovor",
+        "odgovor1": "Naziv prve oblasti: Prvo pitanje - odgovor",
         "odgovor2": "Naziv prve oblasti: Drugo pitanje - odgovor",
         "odgovor3": "Naziv prve oblasti: Treće pitanje - odgovor",
-        "odgovor4": "2. Naziv prve oblasti: Četvrto pitanje - odgovor",
+        "odgovor4": "Naziv prve oblasti: Četvrto pitanje - odgovor",
         "odgovor5": "Naziv prve oblasti: Peto pitanje - odgovor",
         "odgovor6": "Naziv prve oblasti: Šesto pitanje - odgovor",
-        "odgovor7": "3. Naziv prve oblasti: Sedmo pitanje - odgovor",
+        "odgovor7": "Naziv prve oblasti: Sedmo pitanje - odgovor",
         "odgovor8": "Naziv prve oblasti: Osmo pitanje - odgovor",
         "odgovor9": "Naziv prve oblasti: Deveto pitanje - odgovor",
-        "odgovor10": "4. Naziv prve oblasti: Deseto pitanje - odgovor",
+        "odgovor10": "Naziv prve oblasti: Deseto pitanje - odgovor",
     }
 
 
@@ -215,10 +215,11 @@ class TestGenerateDocumentForSubject:
         result = generate_document_for_subject(sample_subject, sample_employee)
 
         # Open created document
-        doc = Document(result)
+        doc = Document()
+        doc.LoadFromFile(str(result))
 
         # Get all text from documents
-        full_text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+        full_text = doc.GetText()
 
         # Assert selcted questions are in document
         assert sample_questions["pitanje4"] in full_text
